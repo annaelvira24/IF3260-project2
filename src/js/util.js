0776e87cc29c60578d357485279ea5218fc86f3d
Dhafin Rayhan <dhafin@students.itb.ac.id>
2021-03-23 21:53:20 +0700

    implement load/save file

--- src/js/util.js
+++ src/js/util.js
@@ -68,3 +68,53 @@ canvasElem.addEventListener('mousedown', (e) =>
 { 
     vec = getMousePosition(canvasElem, e);
 });
+
+function saveData() {
+    var json = JSON.stringify(objects);
+    let dataUri =
+        "data:application/json;charset=utf-8," + encodeURIComponent(json);
+    let exportFileDefaultName = "model.json";
+    
+    let linkElement = document.createElement("a");
+    linkElement.setAttribute("href", dataUri);
+    linkElement.setAttribute("download", exportFileDefaultName);
+    linkElement.click();
+}
+
+function loadData() {
+    let linkElement = document.createElement("input");
+    linkElement.setAttribute("type", "file");
+    linkElement.setAttribute("name", "file-load");
+    linkElement.setAttribute("value", "file-load");
+    linkElement.setAttribute("id", "file-load");
+    linkElement.setAttribute("onchange", "importData()");
+
+    var dynamicParent = document.getElementById("dynamic-parent");
+    dynamicParent.appendChild(linkElement);
+    
+    linkElement.click();
+}
+
+async function importData() {
+    var fileUpload = document.getElementById("file-load");
+
+    if (fileUpload.value !== "") {
+        var path = (window.URL || window.webkitURL).createObjectURL(fileUpload.files[0]);
+        console.log(path);
+        initModelFile(path);
+    }
+}
+
+async function initModelFile(filename) {
+    const modelJson = await loadFile(filename);
+    objects = JSON.parse(modelJson);
+    setUpInitScene();
+}
+
+const loadFile = async (filename) => {
+    return await fetchFile(filename);
+};
+
+async function fetchFile(filename) {
+    return await fetch(filename).then((res) => res.text());
+}
